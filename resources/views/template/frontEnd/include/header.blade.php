b42a6e884cc99c2b16986693f67c3bb7a9b8b1f2
hassan7867 <47236240+hassan7867@users.noreply.github.com>
2020-05-19 00:55:09 +0500

    laravel initial setup with theme embedded

---
+++ resources/views/template/frontEnd/include/header.blade.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+
+
+<!-- Mirrored from templates.bwlthemes.com/smart_fix/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 18 May 2020 17:20:53 GMT -->
+<head>
+    <meta charset="utf-8">
+    <title>SmartFix - Computer Repair Center HTML5 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="SmartFix - Computer Repair Center HTML5 Template">
+    <meta name="author" content="xenioushk">
+    {{--<link rel="shortcut icon" href="images/favicon.png" />--}}
+    <link href="{{ asset('images/favicon.png') }}" rel="shortcut icon" type='text/css'/>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- The styles -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/v4-shims.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/owl.carousel.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/owl.theme.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/animate.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/venobox.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/styles.css') }}"  rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+
+<div id="preloader">
+    <span class="margin-bottom"><img src="{{ asset('images/loader.gif') }}" alt="Loading......" /></span>
+</div>
+
+<!--  HEADER STYLE  02-->
+
+<header class="main-header-2 clearfix" data-sticky_header="true">
+
+    <section class="header-wrapper navgiation-wrapper">
+
+        <div class="main-top-header clearfix">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-6 col-md-6 col-sm-6 text-left ">
+                        <div class="top-bar-link">
+                            <a href="#">Reviews</a> <span class="separator-line"></span>
+                            <a href="#">Warranty</a> <span class="separator-line"></span>
+                            <a href="#">Contact</a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6 col-sm-6 text-right">
+
+
+                        <div class="top-bar-link">
+                            <strong>Support</strong> : +880-1911-623-458, +8801-717-682-743
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="header-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-4 col-sm-12 col-12 header-col-logo">
+                        <div class="header-logo logo">
+                            <a href="index.html">
+                                <img src="{{ asset('images/logo.png') }}" alt="Logo">
+                            </a>
+                        </div><!-- end .logo  -->
+                    </div><!-- end .col-md-3  -->
+
+                    <div class="col-md-8 col-sm-12">
+                        <div class="row">
+
+                            <div class="col-md-4 col-6">
+                                <div class="header-icon-box">
+                                    <div class="icon-container">
+                                        <i class="fa fa-building-o"></i>
+                                    </div>
+                                    <div class="text">
+                                        <span class="head-heading">OFFICE ADDRESS</span>
+                                        <span class="head-content">
+                                                    Saifa Summit, Bangladesh
+                                                </span>
+                                    </div>
+                                </div><!-- end .repair-icon-box  -->
+                            </div><!-- end .col-md-4  -->
+
+                            <div class="col-md-4 col-6">
+                                <div class="header-icon-box">
+                                    <div class="icon-container">
+                                        <i class="fa fa-calendar-check-o"></i>
+                                    </div>
+                                    <div class="text">
+                                        <span class="head-heading">WORKING HOUR</span>
+                                        <span class="head-content">
+                                                    Monday-Friday 9:00-18:00
+                                                </span>
+                                    </div>
+                                </div><!-- end .header-icon-box  -->
+                            </div> <!-- end .col-md-4  -->
+
+                            <div class="col-md-4 col-12 clearfix">
+                                <a class="btn btn-theme btn-square btn-header-cta margin-top-36 float-right" href="appointment.html" title="GET FREE QUOTE">BOOK APPOINTMENT</a>
+                            </div><!-- end .col-md-4  -->
+
+                        </div> <!-- end .row  -->
+                    </div><!-- end .col-md-8  -->
+
+                </div><!-- end .row  -->
+            </div><!-- end .container  -->
+        </div><!-- end header-top  -->
+
+        <!--  New Code For Menu -->
+
+        <div id="menu_area" class="menu-area border-bottom-1p-solid-light">
+            <div class="container">
+                <div class="row">
+                    <nav class="navbar navbar-light navbar-expand-lg mainmenu col-lg-9 col-12">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="active"><a href="#">HOME <span class="sr-only">(current)</span></a></li>
+                                <li><a href="about-us.html" title="About Us">ABOUT US</a></li>
+
+
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SERVICES</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="service.html">SERVICE LISTS</a></li>
+                                        <li><a href="service-details.html">SINGLE SERVICE</a></li>
+                                    </ul>
+                                </li>
+
+
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">PAGES</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="appointment.html">APPOINTMENT</a></li>
+
+                                        <li class="drop">
+                                            <a href="#">OUR TEAMS</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="team.html">TEAM LISTS</a></li>
+                                                <li><a href="single-team.html">SINGLE TEAM</a></li>
+                                            </ul>
+                                        </li>
+
+                                        <li class="drop">
+                                            <a href="#">BLOG</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="blog.html">ALL POSTS</a></li>
+                                                <li><a href="single.html">SINGLE PAGE</a></li>
+                                            </ul>
+                                        </li>
+
+                                        <li class="drop">
+                                            <a href="#">GALLERY</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="gallery-1.html">LAYOUT 01</a></li>
+                                                <li><a href="gallery-2.html">LAYOUT 02</a></li>
+                                                <li><a href="gallery-3.html">LAYOUT 03</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="faq.html"  title="Faq">FAQ</a></li>
+                                        <li><a href="coming-soon.html" title="Coming Soon">COMING SOON</a></li>
+                                        <li><a href="404.html" title="404 Page">404 PAGE</a></li>
+                                        <li class="drop"><a href="#">LEVEL 3</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="#">LEVEL 3.1</a></li>
+                                                <li><a href="#">LEVEL 3.2</a></li>
+                                                <li><a href="#">LEVEL 3.3</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ELEMENTS</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="element-counter.html" title="Counter">COUNTER</a></li>
+                                        <li><a href="element-cta.html" title="CTA">CTA BOX</a></li>
+                                        <li><a href="element-testimonial.html" title="Testimonial">TESTIMONIAL</a></li>
+                                        <li><a href="element-highlights.html" title="Highlights">HIGHLIGHTS</a></li>
+                                        <li><a href="element-info-box.html" title="Infobox">INFO BOX</a></li>
+                                        <li><a href="element-logos.html" title="Logos">LOGOS</a></li>
+                                        <li><a href="element-pricing-table.html" title="Pricing">PRICING TABLE</a></li>
+                                        <li><a href="element-process.html" title="Process">PROCESS</a></li>
+                                        <li><a href="element-service.html" title="Service">SERVICE</a></li>
+                                        <li><a href="element-appointment.html" title="Appointment Form">APPOINTMENT FORM</a></li>
+                                        <li><a href="element-subscribe.html" title="Subscribe">SUBSCRIBE</a></li>
+                                        <li><a href="element-team.html" title="Teams">TEAMS</a></li>
+                                    </ul>
+                                </li>
+
+                                <li><a href="contact.html" title="Contact">CONTACT</a></li>
+
+                            </ul>
+                        </div>
+                    </nav>
+
+
+                    <!--  Social Icon  -->
+
+                    <div class="navbar-header col-lg-3 col-12">
+                        <div class="navbar-contact">
+
+                            <div class="top-bar-social mt-2">
+                                <a href="#">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-instagram"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-youtube"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fa fa-pinterest"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!--  End New Code For Menu -->
+
+    </section>
+
+</header> <!-- end main-header  -->
---
+++ resources/views/template/frontEnd/layout/layout.blade.php
@@ -0,0 +1,3 @@
+@include('template/frontend/include/header')
+@yield('content')
+@include('template/frontend/include/footer')
